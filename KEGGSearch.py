f6ee1f41c7e025e6fbc8e09adc12d91766ff3989
SaidaKhodaee <91787949+SaidaKhodaee@users.noreply.github.com>
2021-10-31 20:37:11 +0330

    Add files via upload

---
+++ KEGGSearch.py
@@ -0,0 +1,39 @@
+import Bio
+from Bio import KEGG
+from Bio.KEGG import REST
+from Bio.KEGG import KGML
+from Bio.KEGG.KGML import KGML_parser
+
+def getOrgRxnsFromKEGG(org):
+	import re
+	idList,discList,difList,signalingPathwaysIndex,sigPathID,reactionList=[],[],[],[],[],[]
+	pathways=REST.kegg_list("pathway", org).read()
+	pathways=pathways.split("\n")
+	for path in pathways:
+		p1=path.split("\t")
+		if len(p1)==2:
+			idList=idList+[p1[0].split(":")[1]]
+			discList=discList+[p1[1]]
+	for disc in discList:
+		if re.search('signaling', disc) is not None:
+			signalingPathwaysIndex=signalingPathwaysIndex+[discList.index(disc)]
+	for index in signalingPathwaysIndex:
+		sigPathID=sigPathID+[idList[index]]
+	pathIdList=list(set(idList)- set(sigPathID))
+	for pathID in pathIdList:
+		result = REST.kegg_get(pathID, "kgml").read()
+		p1=Bio.KEGG.KGML.KGML_parser.read(result)
+		listReacObj = list(p1.reactions)
+		if listReacObj:
+			for reactionObj in listReacObj:
+				reactionList=reactionList+reactionObj.name.split()
+	return reactionList
+
+humanRxnList=getOrgRxnsFromKEGG('hsa')#in here, reference organism is homo sapiens
+#with open('/tezDoctory/tez_/humanRxnList.txt','w') as file:
+#	file.writelines("% s\t" % rxn for rxn in humanRxnList)
+mouseRxnList=getOrgRxnsFromKEGG('mmu')#in here, Target organism is Mus Musculus
+#with open('/pythonCodeForModel_iMM1865/mouseRxnList.txt','w') as file:
+#	file.writelines("% s\t" % rxn for rxn in mouseRxnList)
+candidateMouseSpecRxns=list(set(mouseRxnList)-set(humanRxnList))
+	
